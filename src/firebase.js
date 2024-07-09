9a5c926905ce4a8fb7903b47f0c2d659bbdbae94
14shoh <149202844+14shoh@users.noreply.github.com>
2024-07-09 19:33:42 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,16 +3,21 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB2hzGx6sgsbdS9UY7ArZDjKF7u0BRL7go",
-    authDomain: "shoh-shop.firebaseapp.com",
-    projectId: "shoh-shop",
-    storageBucket: "shoh-shop.appspot.com",
-    messagingSenderId: "1055815376097",
-    appId: "1:1055815376097:web:da796b391d0b5b6e592ba0"
-  };
-  
+  apiKey: "AIzaSyD4whLmLX_6-5CTEclr0O6HyylIo6Fq6fw",
+  authDomain: "shoh-shop-bb535.firebaseapp.com",
+  projectId: "shoh-shop-bb535",
+  storageBucket: "shoh-shop-bb535.appspot.com",
+  messagingSenderId: "629725825897",
+  appId: "1:629725825897:web:d1fd806572667981968bf8"
+};
+
 
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
   
   export { auth, RecaptchaVerifier, signInWithPhoneNumber };
+
+
+
+
+
